9874de6c370b8633410556dcb246b30439508053
Amirhosein Gharaati <amirgh1380@gmail.com>
2022-08-09 19:00:10 +0430

    soft constraints refactor

--- soft_constraints.py
+++ soft_constraints.py
@@ -2,37 +2,49 @@ from models import Student, Schedule
 from models import SLOT_PER_DAY
 from utils import get_student_time_slots
 
-def has_two_consecutive_exams(schedule: Schedule, student: Student) -> int:
+
+def calculate_number_of_two_consecutive_exams(schedule: Schedule, student: Student) -> int:
     times: list[int] = get_student_time_slots(schedule, student)
     count: int = 0
+
     for i in range(1, len(times)):
-        if times[i]//SLOT_PER_DAY == times[i-1]//SLOT_PER_DAY + 1:
+        if times[i] // SLOT_PER_DAY == times[i-1] // SLOT_PER_DAY + 1:
             count += 1
+
     return count
 
 
-def has_three_consecutive_exams(schedule: Schedule, student: Student) -> int:
+def calculate_number_of_three_consecutive_exams(schedule: Schedule, student: Student) -> int:
     times: list[int] = get_student_time_slots(schedule, student)
     count: int = 0
+
     for i in range(2, len(times)):
-        if times[i-2]//SLOT_PER_DAY == times[i-1]//SLOT_PER_DAY - 1 == times[i]//SLOT_PER_DAY - 2:
+        if times[i-2] // SLOT_PER_DAY == times[i-1] // SLOT_PER_DAY - 1 == times[i] // SLOT_PER_DAY - 2:
             count += 1
+
     return count
 
 
-def has_exam_on_holidays(schedule: Schedule, student: Student) -> int:
+def calculate_number_of_exams_on_holidays(schedule: Schedule, student: Student) -> int:
     count: int = 0
+
     for course in student.courses:
         if schedule.get_course_time(course).is_holiday:
             count += 1
+
     return count
 
+
+class Penalty:
+    TWO_CONSECUTIVE_EXAM = 1000
+    THREE_CONSECUTIVE_EXAM = 50000
+    EXAM_ON_HOLIDAY = 10
 
 def calculate_penalty_of_student(schedule: Schedule, student: Student) -> int:
     penalty: int = 0
 
-    penalty += 1000 * has_two_consecutive_exams(schedule, student)
-    penalty += 50000 * has_three_consecutive_exams(schedule, student)
-    penalty += 10 * has_exam_on_holidays(schedule, student)
+    penalty += Penalty.TWO_CONSECUTIVE_EXAM * calculate_number_of_two_consecutive_exams(schedule, student)
+    penalty += Penalty.THREE_CONSECUTIVE_EXAM * calculate_number_of_three_consecutive_exams(schedule, student)
+    penalty += Penalty.EXAM_ON_HOLIDAY * calculate_number_of_exams_on_holidays(schedule, student)
 
     return penalty
