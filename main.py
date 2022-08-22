add956ec284b44bd5b81372fce6ca1f09e24a758
Arefeh Ahmadi <arefehahmadi902@gmail.com>
2022-08-22 23:55:19 +0430

    update gitignore

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS
 from flask_socketio import SocketIO
 from models import Course, Student, TimeSlot, Schedule
 from read_data import read_courses_data, read_students_data, read_professors_data, read_time_slots_data
 from ga import GeneticAlgorithm
 from constraints.soft_constraints import calculate_penalty_of_student
+from utils import convert_csv_to_xlsx
 
 app = Flask("Exam Scheduling")
 CORS(app)
@@ -20,7 +21,7 @@ def get_schedule():
     time_slots: list[TimeSlot] = read_time_slots_data(content["time_slots"])
     available_time_slots = TimeSlot.get_available_time_slots(time_slots)
     hyper_parameters_list: list[dict[str, int]] = content["hyper_parameters_list"]
-    slot_per_day: int = content['number_of_slots_per_day']
+    slot_per_day: int = content["number_of_slots_per_day"]
     Schedule.SLOT_PER_DAY = slot_per_day
     TimeSlot.SLOT_PER_DAY = slot_per_day
 
@@ -60,6 +61,7 @@ def get_schedule():
         print('----------------------------')
 
     last_schedule.print()
+    # convert_csv_to_xlsx(last_schedule.get_csv_export())
     return last_schedule.to_json()
 
 
