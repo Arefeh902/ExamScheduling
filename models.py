8a834447dadaba9fbf6c778d2795d9baceff3ca0
Arefeh Ahmadi <arefehahmadi902@gmail.com>
2023-02-27 17:29:55 +0330

    fixed bug

--- models.py
+++ models.py
@@ -126,11 +126,11 @@ class Schedule:
         self.exams_on_holiday = 0
         self.single_day_rest = 0
 
-        students_with_two_exams_in_one_day: int
-        students_with_two_consecutive_exams: int
-        students_with_three_consecutive_exams: int
-        students_with_exams_on_holiday: int
-        students_with_single_day_rest: int
+        self.students_with_two_exams_in_one_day = 0
+        self.students_with_two_consecutive_exams = 0
+        self.students_with_three_consecutive_exams = 0
+        self.students_with_exams_on_holiday = 0
+        self.students_with_single_day_rest = 0
         for slot in time_slots:
             self.time_to_course[slot] = list()
 
